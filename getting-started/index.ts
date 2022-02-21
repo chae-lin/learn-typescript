a25bc69228a9cf4b1e9b8b2799c9a30cb67fd550
chaelin kim <gjf0055@gmail.com>
2022-02-21 15:50:59 +0900

    chore: 수정

--- getting-started/index.ts
+++ getting-started/index.ts
@@ -6,6 +6,6 @@ sum2(10, 20);
 
 /* 
 컴파일 하기
-    해당파일 마우스 우클릭하여 터미널: npm i typescript -g
-    타입스크립트 파일 자바스크립트 파일로 변환하기: tsc index.ts 
+  해당파일 마우스 우클릭하여 터미널: npm i typescript -g
+  타입스크립트 파일 자바스크립트 파일로 변환하기: tsc index.ts 
 */
